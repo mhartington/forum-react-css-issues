170b3ca4dc4b36f76b7ad3954e41207a3e6fc8b4
Mike Hartington <mikehartington@gmail.com>
2021-06-10 10:37:04 -0400

    add demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -10,13 +16,13 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonButton fill="clear" class="nav-button">
+          About
+        </IonButton>
+          <IonButton fill="clear" class="nav-button">
+            Posts
+          </IonButton>
       </IonContent>
     </IonPage>
   );
